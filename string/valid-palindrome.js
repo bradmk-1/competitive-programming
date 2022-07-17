43f55b6fec0acd3820eb2f57e6914d7a18443108
Brad <bradleyjm2001@gmail.com>
2022-07-17 20:00:11 +0100

    Valid Palindrome solution

---
+++ string/valid-palindrome.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = (s) => {
+    const cleanedString = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    
+    if (cleanedString.split('').reverse().join('') === cleanedString) {
+        return true;
+    };
+    
+    return false;
+};
