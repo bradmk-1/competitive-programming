48325d0df96640a83d9a544cbd2b6a8ad1916ae5
Brad <bradleyjm2001@gmail.com>
2022-07-23 22:56:19 +0100

    Best time to buy and sell stock solution

---
+++ array/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+const maxProfit = (prices) => {
+    let lowestDay = Infinity;
+    let highestDay = 0;
+    
+    for (i = 0; i < prices.length; i++) {
+        lowestDay = Math.min(lowestDay, prices[i]);
+        highestDay = Math.max(highestDay, prices[i] - lowestDay);
+    }
+    
+    return highestDay;
+
+}; 
+
+
+//Brute force with two for loops, for every value in prices calculate value - value[i] to the right of it
