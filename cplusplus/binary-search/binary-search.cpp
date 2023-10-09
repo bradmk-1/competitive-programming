0e390874f8a42e5c75843ed122885c47316aeeb2
Brad Moore <bradleyjm2001@gmail.com>
2023-10-09 20:15:42 +0100

    binary search solution (c++)

---
+++ cplusplus/binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  int search(vector<int>& nums, int target) {
+    int startPtr = 0;
+    int endPtr = nums.size() - 1;
+    int midPtr = 0;
+
+    while (startPtr <= endPtr) {
+      midPtr = (startPtr + endPtr) / 2;
+      if (nums[midPtr] == target) {
+        return midPtr;
+      } else if (nums[midPtr] < target) {
+        startPtr = midPtr + 1;
+      } else if (nums[midPtr] > target) {
+        endPtr = midPtr - 1;
+      }
+    };
+
+    return -1;
+  }
+};
