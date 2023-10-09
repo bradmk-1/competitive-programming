7b9a26a16d1df08bbb862684a5436f870e80ebc2
Brad Moore <bradleyjm2001@gmail.com>
2023-10-09 20:17:48 +0100

    fix linting

--- cplusplus/binary-search/binary-search.cpp
+++ cplusplus/binary-search/binary-search.cpp
@@ -1,21 +1,23 @@
 class Solution {
- public:
-  int search(vector<int>& nums, int target) {
-    int startPtr = 0;
-    int endPtr = nums.size() - 1;
-    int midPtr = 0;
+public:
+    int search(vector<int>& nums, int target) {
+        int startPtr = 0;
+        int endPtr = nums.size() - 1;
+        int midPtr = 0;
 
-    while (startPtr <= endPtr) {
-      midPtr = (startPtr + endPtr) / 2;
-      if (nums[midPtr] == target) {
-        return midPtr;
-      } else if (nums[midPtr] < target) {
-        startPtr = midPtr + 1;
-      } else if (nums[midPtr] > target) {
-        endPtr = midPtr - 1;
-      }
-    };
+        while (startPtr <= endPtr) {
+            midPtr = (startPtr + endPtr) / 2;
+            if (nums[midPtr] == target) {
+                return midPtr;
+            }
+            else if (nums[midPtr] < target) {
+                startPtr = midPtr + 1;
+            }
+            else if (nums[midPtr] > target) {
+                endPtr = midPtr - 1;
+            }
+        };
 
-    return -1;
-  }
+        return -1;
+    }
 };
