e3f9f925813ed303d095524ab0b39535653366bd
bradmk-1 <bradleyjm2001@gmail.com>
2025-12-22 22:55:48 +0000

    binary tree level order traversal ii solution (java)

---
+++ java/trees/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public void bfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int level = queue.size();
+
+            List<Integer> row = new ArrayList<>();
+
+            for (int i = 0; i < level; i++) {
+                var curr = queue.poll();
+
+                row.add(curr.val);
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+
+            res.addFirst(row);
+        }
+
+
+
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        bfs(root);
+        return res;
+    }
+}
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
