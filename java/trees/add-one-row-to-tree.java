1dd09ae3b7d07769ff9803a560283444c9e1c248
bradmk-1 <bradleyjm2001@gmail.com>
2026-02-24 21:46:39 +0000

    add one row to tree solution (java)

---
+++ java/trees/add-one-row-to-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode dfs(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return root;
+        }
+        
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        }
+
+        dfs(root.left, val, depth - 1);
+        dfs(root.right, val, depth - 1);
+
+        return root;
+    }
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        } else {
+            return dfs(root, val, depth);            
+        }
+    }
+}
+
+// https://leetcode.com/problems/add-one-row-to-tree
